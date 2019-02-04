c0e62cc72fdda1481a9ca1de4f5ea97fd8cad5af
W.May <wmay@pixelsystems.net>
2019-02-04 07:53:27 -0700

    Create HelloWorld.java

---
+++ src/net/pixelsystems/HelloWorld.java
@@ -0,0 +1,8 @@
+package net.pixelsystems;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
